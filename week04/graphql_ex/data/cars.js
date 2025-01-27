c20473ada4b2d66d3febfb21795bf28658733b61
Tim Robillard <hello@timrobillard.ca>
2025-01-27 17:28:28 -0500

    graphql example

---
+++ week04/graphql_ex/data/cars.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const cars = [
+  { id: 1, make: "Tesla", model: "S", colour: "Black" },
+  { id: 2, make: "Tesla", model: "3", colour: "Red" },
+  { id: 3, make: "Toyota", model: "Prius V", colour: "Silver" },
+  { id: 4, make: "Ford", model: "F150", colour: "Chestnut Brown" },
+];
+
+module.exports = cars;
---
+++ week04/graphql_ex/queries.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const {
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLError,
+  GraphQLInputObjectType,
+  GraphQLString,
+} = require("graphql");
+
+const cars = require("./data/cars");
+const drivers = require("./data/drivers");
+const { DriverType, CarType } = require("./types");
+
+const getCar = {
+  type: CarType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+  resolve: (_source, { id }) => {
+    const car = cars.find((car) => car.id === id);
+    if (!car) throw new GraphQLError(`Car with id ${id} not found`);
+
+    return car;
+  },
+};
+
+const getCars = {
+  type: new GraphQLList(CarType),
+  args: {
+    filter: {
+      type: new GraphQLInputObjectType({
+        name: "GetCarsFilter",
+        fields: {
+          make: {
+            type: GraphQLString,
+          },
+        },
+      }),
+    },
+  },
+  resolve: (_, { filter }) => {
+    if (filter?.make) {
+      return cars.filter((car) => car.make === filter.make);
+    }
+    return cars;
+  },
+};
+
+const getDriver = {
+  type: DriverType,
+  args: {
+    id: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+  resolve: (_source, { id }) => {
+    const driver = drivers.find((driver) => driver.id === id);
+    if (!driver) throw new GraphQLError(`Driver with id ${id} not found`);
+    return driver;
+  },
+};
+
+const getDrivers = {
+  type: new GraphQLList(DriverType),
+  resolve: () => drivers,
+};
+
+const query = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    car: getCar,
+    cars: getCars,
+    driver: getDriver,
+    drivers: getDrivers,
+  },
+});
+
+module.exports = query;
